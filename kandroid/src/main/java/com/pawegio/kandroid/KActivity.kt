d98706fbce2cea6b54bd64bddc72efbcabee2c7e
Paweł Gajda <pawegio@gmail.com>
2015-03-28 09:31:29 +0100

    Optimizes in imports

--- kandroid/src/main/java/com/pawegio/kandroid/KActivity.kt
+++ kandroid/src/main/java/com/pawegio/kandroid/KActivity.kt
@@ -1,45 +1,7 @@
 package com.pawegio.kandroid
 
-import android.accounts.AccountManager
-import android.app.*
-import android.app.admin.DevicePolicyManager
-import android.app.job.JobScheduler
-import android.app.usage.UsageStatsManager
-import android.appwidget.AppWidgetManager
-import android.bluetooth.BluetoothManager
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.RestrictionsManager
-import android.content.pm.LauncherApps
-import android.hardware.ConsumerIrManager
-import android.hardware.SensorManager
-import android.hardware.camera2.CameraManager
-import android.hardware.display.DisplayManager
-import android.hardware.input.InputManager
-import android.hardware.usb.UsbManager
-import android.location.LocationManager
-import android.media.AudioManager
-import android.media.MediaRouter
-import android.media.projection.MediaProjectionManager
-import android.media.session.MediaSessionManager
-import android.media.tv.TvInputManager
-import android.net.ConnectivityManager
-import android.net.nsd.NsdManager
-import android.net.wifi.WifiManager
-import android.net.wifi.p2p.WifiP2pManager
-import android.nfc.NfcManager
-import android.os.*
-import android.os.storage.StorageManager
-import android.print.PrintManager
-import android.telecom.TelecomManager
-import android.telephony.TelephonyManager
-import android.view.LayoutInflater
+import android.app.Activity
 import android.view.View
-import android.view.WindowManager
-import android.view.accessibility.AccessibilityManager
-import android.view.accessibility.CaptioningManager
-import android.view.inputmethod.InputMethodManager
-import android.view.textservice.TextServicesManager
 import android.widget.Toast
 
 /**
