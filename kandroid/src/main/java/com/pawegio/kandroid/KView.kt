13f055487903e4264fffb8674033401d5ca9deff
Paweł Gajda <pawegio@gmail.com>
2015-04-26 00:00:20 +0200

    Removed deprecated findView

--- kandroid/src/main/java/com/pawegio/kandroid/KView.kt
+++ kandroid/src/main/java/com/pawegio/kandroid/KView.kt
@@ -7,7 +7,3 @@ import android.view.View
  */
 [suppress("UNCHECKED_CAST")]
 public fun View.find<T : View>(id: Int): T = findViewById(id) as T
-
-[suppress("UNCHECKED_CAST")]
-deprecated("Use View.find() instead")
-public fun View.findView<T : View>(id: Int): T = findViewById(id) as T
