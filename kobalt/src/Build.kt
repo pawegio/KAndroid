953f6ad3b913a33865b95baf477999c910aafbb6
Paweł Gajda <pawegio@gmail.com>
2017-02-14 22:59:16 +0100

    Configure android plugin and add dependencies

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,6 +1,7 @@
 import com.beust.kobalt.buildScript
 import com.beust.kobalt.plugin.packaging.assemble
 import com.beust.kobalt.project
+import com.beust.kobalt.plugin.android.*
 
 val kandroidVersion = "0.6.16"
 
@@ -15,6 +16,21 @@ val p = project {
     artifactId = name
     version = kandroidVersion
     directory = "kandroid"
+
+    android {
+        defaultConfig {
+            minSdkVersion = 16
+            versionCode = 1
+            versionName = kandroidVersion
+            compileSdkVersion = "25"
+            buildToolsVersion = "25.0.1"
+            applicationId = "com.pawegio.kandroid"
+        }
+    }
+
+    dependencies {
+        compile("com.android.support:appcompat-v7:25.1.0")
+    }
 
     assemble {
         jar {
