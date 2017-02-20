dd9093ff406e6a038dc6a0f1681e24ef203f21ba
Paweł Gajda <pawegio@gmail.com>
2017-02-20 16:46:14 +0100

    Update kobalt and kobalt-android

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -5,7 +5,7 @@ import com.beust.kobalt.plugin.android.*
 val kandroidVersion = "0.6.16"
 
 val bs = buildScript {
-    plugins("com.beust:kobalt-android:0.95")
+    plugins("com.beust:kobalt-android:0.96")
 }
 
 val p = project {
