32a3338e758bc32761d908c464cb0ef4f4958e88
illuzor <illuzor@gmail.com>
2018-02-07 14:42:37 +0300

    RequiresApi annotation added

--- kandroid/src/main/kotlin/com/pawegio/kandroid/KContext.kt
+++ kandroid/src/main/kotlin/com/pawegio/kandroid/KContext.kt
@@ -51,6 +51,7 @@ import android.os.storage.StorageManager
 import android.preference.PreferenceManager
 import android.print.PrintManager
 import android.support.annotation.LayoutRes
+import android.support.annotation.RequiresApi
 import android.support.annotation.StringRes
 import android.support.v4.content.ContextCompat
 import android.telecom.TelecomManager
@@ -86,24 +87,30 @@ inline val Context.alarmManager
     get() = getSystemService(Context.ALARM_SERVICE) as AlarmManager?
 
 inline val Context.appWidgetManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.APPWIDGET_SERVICE) as AppWidgetManager?
 
 inline val Context.appOpsManager
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     get() = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager?
 
 inline val Context.audioManager
     get() = getSystemService(Context.AUDIO_SERVICE) as AudioManager?
 
 inline val Context.batteryManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.BATTERY_SERVICE) as BatteryManager?
 
 inline val Context.bluetoothManager
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     get() = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager?
 
 inline val Context.cameraManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.CAMERA_SERVICE) as CameraManager?
 
 inline val Context.captioningManager
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     get() = getSystemService(Context.CAPTIONING_SERVICE) as CaptioningManager?
 
 inline val Context.clipboardManager
@@ -113,12 +120,14 @@ inline val Context.connectivityManager
     get() = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager?
 
 inline val Context.consumerIrManager
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     get() = getSystemService(Context.CONSUMER_IR_SERVICE) as ConsumerIrManager?
 
 inline val Context.devicePolicyManager
     get() = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager?
 
 inline val Context.displayManager
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     get() = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager?
 
 inline val Context.downloadManager
@@ -134,12 +143,14 @@ inline val Context.inputManager
     get() = getSystemService(Context.INPUT_SERVICE) as InputManager?
 
 inline val Context.jobScheduler
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler?
 
 inline val Context.keyguardManager
     get() = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager?
 
 inline val Context.launcherApps
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.LAUNCHER_APPS_SERVICE) as LauncherApps?
 
 inline val Context.layoutInflater
@@ -149,12 +160,14 @@ inline val Context.locationManager
     get() = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
 
 inline val Context.mediaProjectionManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager?
 
 inline val Context.mediaRouter
     get() = getSystemService(Context.MEDIA_ROUTER_SERVICE) as MediaRouter?
 
 inline val Context.mediaSessionManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.MEDIA_SESSION_SERVICE) as MediaSessionManager?
 
 inline val Context.nfcManager
@@ -170,9 +183,11 @@ inline val Context.powerManager
     get() = getSystemService(Context.POWER_SERVICE) as PowerManager?
 
 inline val Context.printManager
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     get() = getSystemService(Context.PRINT_SERVICE) as PrintManager?
 
 inline val Context.restrictionsManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.RESTRICTIONS_SERVICE) as RestrictionsManager?
 
 inline val Context.searchManager
@@ -185,6 +200,7 @@ inline val Context.storageManager
     get() = getSystemService(Context.STORAGE_SERVICE) as StorageManager?
 
 inline val Context.telecomManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.TELECOM_SERVICE) as TelecomManager?
 
 inline val Context.telephonyManager
@@ -194,6 +210,7 @@ inline val Context.textServicesManager
     get() = getSystemService(Context.TEXT_SERVICES_MANAGER_SERVICE) as TextServicesManager?
 
 inline val Context.tvInputManager
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     get() = getSystemService(Context.TV_INPUT_SERVICE) as TvInputManager?
 
 inline val Context.uiModeManager
@@ -203,6 +220,7 @@ inline val Context.usbManager
     get() = getSystemService(Context.USB_SERVICE) as UsbManager?
 
 inline val Context.userManager
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     get() = getSystemService(Context.USER_SERVICE) as UserManager?
 
 inline val Context.vibrator
